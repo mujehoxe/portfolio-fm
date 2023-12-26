61d03dc80d63044de8126b82f910e0f14958e222
muje hoxe <mujehoxe@gmail.com>
2023-12-27 00:41:08 +0100

    nav bar

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("burgerIcon").addEventListener("click", function () {
+  document.getElementById("navList").classList.toggle("hidden");
+  document.getElementById("closeNav").classList.toggle("hidden");
+  document.getElementById("openNav").classList.toggle("hidden");
+  document.getElementById("contactList").classList.toggle("hidden");
+});
