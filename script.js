73c948479508bd9dbe0adcfa3a247e8474cd1170
muje hoxe <mujehoxe@gmail.com>
2023-12-27 01:25:51 +0100

    fixed nav; added smile image

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ document.getElementById("burgerIcon").addEventListener("click", function () {
   document.getElementById("closeNav").classList.toggle("hidden");
   document.getElementById("openNav").classList.toggle("hidden");
   document.getElementById("contactList").classList.toggle("hidden");
+  document.getElementsByTagName("main")[0].classList.toggle("hidden");
+  document.getElementsByTagName("nav")[0].classList.toggle("h-screen");
 });
