61b2c0335cad7d3efa5b274a442f3adc85777a84
muje hoxe <mujehoxe@gmail.com>
2024-01-03 11:43:36 +0100

    hide footer on menu open

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.getElementById("burgerIcon").addEventListener("click", function () {
   document.getElementById("openNav").classList.toggle("hidden");
   document.getElementById("contactList").classList.toggle("hidden");
   document.getElementsByTagName("main")[0].classList.toggle("hidden");
+  document.getElementsByTagName("footer")[0].classList.toggle("hidden");
   document.getElementsByTagName("nav")[0].classList.toggle("h-screen");
 });
 
